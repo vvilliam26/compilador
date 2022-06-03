6465b54e53fb408ccb73510a3c38eed650ef44fa
vvilliam26 <51523303+vvilliam26@users.noreply.github.com>
2022-06-02 23:08:35 -0300

    ident_automato start

--- Compilador/main.c
+++ Compilador/main.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+
+
+char* automato_ident(FILE *pos) {
+    FILE *aux
+    aux = pos;
+    char id [100];
+    char buffer
+
+    //Estado q0
+    fread(&buffer, sizeof(char), 1, aux);
+
+
+    if((buffer >= 66 && buffer <= 90) || (buffer >= 97 && buffer <= 122))
+    {
+        //Primeiro char eh letra
+
+        //Estado q1
+        while(()(buffer >= 66 && buffer <= 90) || (buffer >= 97 && buffer <= 122))
+
+    } else {
+        print("Erro");
+    }
+
+
+
+
+}
+
 
 int main()
 {
