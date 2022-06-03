041a5d21e92ec090ba78d5cb241dcc791a5a04d8
vvilliam26 <51523303+vvilliam26@users.noreply.github.com>
2022-06-03 19:25:57 -0300

    automato palavra reservado

--- Compilador/main.c
+++ Compilador/main.c
@@ -2,43 +2,44 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *automato_operadores_relacionais(FILE *pos) {
-    FILE *aux;
-    aux = pos;
-    char symbol[2];
-    char buffer;
-    //Estado q0
-    fread(&buffer, sizeof(char), 1, aux);
-    if (buffer == '<') {//Estado q1
-        strcat(symbol,buffer);
-        fread(&buffer, sizeof(char), 1, aux);
-        if (buffer == '=') {//Estado q2
-            strcat(symbol,buffer);// <= simb_meig
-        } else if (buffer == '>') {//Estado q3
-            strcat(symbol,buffer);// <> simb_dif
-        } else {//Estado q4
-            // > simb_menor
-        }
-    } else if (buffer == '=') {// Estado q5
-        strcat(symbol,buffer);// = simb_ig
-    } else if (buffer == '>') {// Estado q6
-        fread(&buffer, sizeof(char), 1, aux);
-        strcat(symbol,buffer);
-        if (buffer == '=') {// Estado q7
-            strcat(symbol,buffer);// <= simb_meig
-        } else {//Estado q8
-            strcat(symbol,buffer);// > simb_maior
-            // retroceder
-        }
-    }
-    return symbol;
-}
+//char *automato_operadores_relacionais(FILE *pos) {
+//    FILE *aux;
+//    aux = pos;
+//    char symbol[2];
+//    char buffer;
+//    //Estado q0
+//    fread(&buffer, sizeof(char), 1, aux);
+//    if (buffer == '<') {//Estado q1
+//        strcat(symbol,buffer);
+//        fread(&buffer, sizeof(char), 1, aux);
+//        if (buffer == '=') {//Estado q2
+//            strcat(symbol,buffer);// <= simb_meig
+//        } else if (buffer == '>') {//Estado q3
+//            strcat(symbol,buffer);// <> simb_dif
+//        } else {//Estado q4
+//            // > simb_menor
+//        }
+//    } else if (buffer == '=') {// Estado q5
+//        strcat(symbol,buffer);// = simb_ig
+//    } else if (buffer == '>') {// Estado q6
+//        fread(&buffer, sizeof(char), 1, aux);
+//        strcat(symbol,buffer);
+//        if (buffer == '=') {// Estado q7
+//            strcat(symbol,buffer);// <= simb_meig
+//        } else {//Estado q8
+//            strcat(symbol,buffer);// > simb_maior
+//            // retroceder
+//        }
+//    }
+//    return symbol;
+//}
 
-char *automato_ident(FILE *pos) {
+int automato_ident(FILE *pos, char* token[2]) {
     FILE *aux;
     aux = pos;
     char id[100];
     char buffer;
+    char simb[] = "ident";
 
     //Estado q0
     fread(&buffer, sizeof(char), 1, aux);
@@ -46,27 +47,62 @@ char *automato_ident(FILE *pos) {
 
     if ((buffer >= 66 && buffer <= 90) || (buffer >= 97 && buffer <= 122)) {
         //Primeiro char eh letra
-        strcat(id, buffer);
+        strcat(id, &buffer);
 
+        fread(&buffer, sizeof(char), 1, aux); //move o ponteiro
         //Estado q1
         while((buffer >= 48 && buffer <= 57) || (buffer >= 66 && buffer <= 90) || (buffer >= 97 && buffer <= 122))
         {
+            strcat(id, &buffer);
+            //caractere lido eh letra ou digito
             fread(&buffer, sizeof(char), 1, aux);
-            strcat(id, buffer);
+
         }
 
-        if(BATE_HASH(buffer) == true)
+        if(/*BATE_HASH(buffer) == 1*/1)
         {
+            token[0] = id;
+            token[1] = simb;
 
+            return 1;
         }
 
     } else {
-        print("Identificador mal formatado");
+        printf("Identificador mal formatado");
+        return 0;
     }
 
-
+    return 0;
 }
 
+
+int automato_palavraReservada(FILE *pos, char* token) {
+    FILE *aux;
+    aux = pos;
+    char palavra[100];
+    char buffer;
+    char *simb;
+
+    fread(&buffer, sizeof(char), 1, aux);
+
+    while((buffer >= 66 && buffer <= 90) || (buffer >= 97 && buffer <= 122))
+    {
+        //caractere lido eh letra
+        strcat(simb, &buffer);
+        fread(&buffer, sizeof(char), 1, aux);
+    }
+
+    if(/*BATE_HASH(palavra, &simb) == 1 */) {
+        token[0] = palavra;
+        token[1] = simb;
+
+        return 1;
+    } else {
+        return 0;
+    }
+
+    return 0;
+}
 
 int main() {
     printf("Hello world!\n");
